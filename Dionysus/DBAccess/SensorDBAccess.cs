7d811b5f250810b291f1f7be4c56b8e4468cb575
HarryTomovski <43003754+HarryTomovski@users.noreply.github.com>
2021-12-01 16:53:46 +0100

    Added DI for sensor

--- Dionysus/DBAccess/SensorDBAccess.cs
+++ Dionysus/DBAccess/SensorDBAccess.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using Dionysus.DBAccess.Interfaces;
+using Dionysus.DBModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Dionysus.DBAccess
 {
-    public class SensorDBAccess
+    public class SensorDBAccess : ISensorDBAccess
     {
+        public async Task<int> addSensor(Sensor sensor)
+        {
+            using (var context = new DionysusContext())
+            {
+                try
+                {
+                    await context.Sensors.AddAsync(sensor);
+                    await context.SaveChangesAsync();
+                    return sensor.SensorPinNumber;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return -1;
+                }
+            }
+        }
     }
 }
